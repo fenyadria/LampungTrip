9dbce7124fec3fc32890ae3903241dec26abf074
misele <miseleaisya@gmail.com>
2026-05-02 12:26:41 +0700

    edit css

--- login.php
+++ login.php
@@ -38,7 +38,7 @@ if (isset($_POST['login'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
 
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 
